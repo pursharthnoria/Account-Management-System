59343b47e2910b3e427324d41f778d7dc7143b06
Pursharth <pursharth.nohria007@gmail.com>
2022-04-11 10:26:08 +0530

    initial commit

---
+++ src/test/java/com/bankManagement/Barclays/BankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankManagement.Barclays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
