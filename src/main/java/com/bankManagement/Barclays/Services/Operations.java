d7ea9d4c7a1db223d581b6c16c3188f657397d4c
Pursharth <pursharth.nohria007@gmail.com>
2022-04-11 11:59:01 +0530

    added service layer.

---
+++ src/main/java/com/bankManagement/Barclays/Services/Operations.java
@@ -0,0 +1,6 @@
+package com.bankManagement.Barclays.Services;
+
+public class Operations {
+    // import the classes from the package users here
+    // Write the functions that are required by Prerna here in this class.
+}
