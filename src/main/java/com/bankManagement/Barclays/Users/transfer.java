8c6bd2f7d1219ab8e538ad04ae589ee2f05f7ed9
pranav <pranavrayapatin@gmail.com>
2022-04-11 11:39:38 +0530

    getter and setters added

--- src/main/java/com/bankManagement/Barclays/Users/transfer.java
+++ src/main/java/com/bankManagement/Barclays/Users/transfer.java
@@ -2,7 +2,43 @@ package com.bankManagement.Barclays.Users;
 
 public class transfer {
 
-    String fromAccountNumber;
+    public String getFromAccountNumber() {
+		return fromAccountNumber;
+	}
+	public void setFromAccountNumber(String fromAccountNumber) {
+		this.fromAccountNumber = fromAccountNumber;
+	}
+	public String getToAccountNumber() {
+		return toAccountNumber;
+	}
+	public void setToAccountNumber(String toAccountNumber) {
+		this.toAccountNumber = toAccountNumber;
+	}
+	public float getAmount() {
+		return amount;
+	}
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+	public String getTransactionReferenceNumber() {
+		return transactionReferenceNumber;
+	}
+	public void setTransactionReferenceNumber(String transactionReferenceNumber) {
+		this.transactionReferenceNumber = transactionReferenceNumber;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public float getCurrentBalance() {
+		return currentBalance;
+	}
+	public void setCurrentBalance(float currentBalance) {
+		this.currentBalance = currentBalance;
+	}
+	String fromAccountNumber;
     String toAccountNumber;
     float amount;
     String transactionReferenceNumber;
