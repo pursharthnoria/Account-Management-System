defe14dce94292481e52df881e27b158a2488753
prerna2903 <Feb22-at-745114893577>
2022-04-11 21:43:53 +0530

    deposit functionality

--- src/main/java/com/bankManagement/Barclays/Services/Operations.java
+++ src/main/java/com/bankManagement/Barclays/Services/Operations.java
@@ -10,11 +10,14 @@ import com.bankManagement.Barclays.Users.BankCustomers;
 import com.bankManagement.Barclays.Users.Transaction;
 import com.bankManagement.Barclays.repository.BankRepository;
 
+@Service
 public class Operations {
     // import the classes from the package users here
     // Write the functions that are required by Prerna here in this class.
+	
     @Autowired
     BankRepository repo;
+    
     public String accountCreation(BankCustomers customer) {
         return repo.accountCreation(customer, generateCustomerId(), generatePassword());
     }
@@ -22,6 +25,15 @@ public class Operations {
     public List<Transaction> fiveTransaction(String fromAccount){
     	return repo.fiveTransaction(fromAccount);
     
+    }
+    
+    public String deposit(String accountNumber, int amount) {
+    	if(repo.deposit(accountNumber, amount)=="True") {
+    		String result=amount+" is successfully deposited to account "+accountNumber;
+    		return result;
+    	}else {
+    		return "Amount is not deposited due to some error please try again";
+    	}
     }
 
 
