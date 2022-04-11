a6d81563554cb0b3809fc298de7c71694b66e60e
Pursharth <pursharth.nohria007@gmail.com>
2022-04-11 16:36:22 +0530

    added database operations

--- src/main/java/com/bankManagement/Barclays/Services/Operations.java
+++ src/main/java/com/bankManagement/Barclays/Services/Operations.java
@@ -3,4 +3,5 @@ package com.bankManagement.Barclays.Services;
 public class Operations {
     // import the classes from the package users here
     // Write the functions that are required by Prerna here in this class.
+
 }
