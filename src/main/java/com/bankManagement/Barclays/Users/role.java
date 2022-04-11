0e407f6a822a6abca96f13f77e88eefd39c247a7
pranav <pranavrayapatin@gmail.com>
2022-04-11 13:16:44 +0530

    code readble

--- src/main/java/com/bankManagement/Barclays/Users/role.java
+++ src/main/java/com/bankManagement/Barclays/Users/role.java
@@ -1,18 +1,23 @@
 package com.bankManagement.Barclays.Users;
 
 public class role {
+	
+	int roleId;
+    String name;
+    
     public int getRoleId() {
 		return roleId;
 	}
 	public void setRoleId(int roleId) {
 		this.roleId = roleId;
 	}
+	
+	
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-	int roleId;
-    String name;
+	
 }
--- src/main/java/com/bankManagement/Barclays/Users/transfer.java
+++ src/main/java/com/bankManagement/Barclays/Users/transfer.java
@@ -1,6 +1,13 @@
 package com.bankManagement.Barclays.Users;
 
 public class transfer {
+	
+	String fromAccountNumber;
+    String toAccountNumber;
+    float amount;
+    String transactionReferenceNumber;
+    String type;                                //Debit or credit
+    float currentBalance;
 
     public String getFromAccountNumber() {
 		return fromAccountNumber;
@@ -8,41 +15,42 @@ public class transfer {
 	public void setFromAccountNumber(String fromAccountNumber) {
 		this.fromAccountNumber = fromAccountNumber;
 	}
+	
 	public String getToAccountNumber() {
 		return toAccountNumber;
 	}
 	public void setToAccountNumber(String toAccountNumber) {
 		this.toAccountNumber = toAccountNumber;
 	}
+	
 	public float getAmount() {
 		return amount;
 	}
 	public void setAmount(float amount) {
 		this.amount = amount;
 	}
+	
 	public String getTransactionReferenceNumber() {
 		return transactionReferenceNumber;
 	}
 	public void setTransactionReferenceNumber(String transactionReferenceNumber) {
 		this.transactionReferenceNumber = transactionReferenceNumber;
 	}
+	
 	public String getType() {
 		return type;
 	}
 	public void setType(String type) {
 		this.type = type;
 	}
+	
+	
 	public float getCurrentBalance() {
 		return currentBalance;
 	}
 	public void setCurrentBalance(float currentBalance) {
 		this.currentBalance = currentBalance;
 	}
-	String fromAccountNumber;
-    String toAccountNumber;
-    float amount;
-    String transactionReferenceNumber;
-    String type;                                //Debit or credit
-    float currentBalance;
+
 
 }
