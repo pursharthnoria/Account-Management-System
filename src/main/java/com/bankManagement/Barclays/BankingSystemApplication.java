7b62cd4f8227ebd0303249bde67c08ab96d56ce0
Pursharth <pursharth.nohria007@gmail.com>
2022-04-12 23:20:23 +0530

    removed tomcat

--- src/main/java/com/bankManagement/Barclays/BankingSystemApplication.java
+++ src/main/java/com/bankManagement/Barclays/BankingSystemApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BankingSystemApplication extends SpringBootServletInitializer{
+public class BankingSystemApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankingSystemApplication.class, args);
--- src/main/java/com/bankManagement/Barclays/controller/BankController.java
+++ src/main/java/com/bankManagement/Barclays/controller/BankController.java
@@ -5,6 +5,7 @@ import com.bankManagement.Barclays.Services.Operations;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
