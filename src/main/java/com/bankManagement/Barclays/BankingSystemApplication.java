bf322be418552c9f7ff98476728b8decef267c41
prerna2903 <Feb22-at-745114893577>
2022-04-13 11:00:08 +0530

    mail functionality

--- src/main/java/com/bankManagement/Barclays/BankingSystemApplication.java
+++ src/main/java/com/bankManagement/Barclays/BankingSystemApplication.java
@@ -2,9 +2,16 @@ package com.bankManagement.Barclays;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BankingSystemApplication{
+public class BankingSystemApplication extends SpringBootServletInitializer{
+	
+	@Override
+	   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	      return application.sources(BankingSystemApplication.class);
+	   }
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankingSystemApplication.class, args);
--- src/main/java/com/bankManagement/Barclays/controller/BankController.java
+++ src/main/java/com/bankManagement/Barclays/controller/BankController.java
@@ -4,6 +4,8 @@ import com.bankManagement.Barclays.Services.Operations;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,6 +30,8 @@ public class BankController{
 
 	@Autowired
 	Operations operations;
+	
+	Logger logger=LoggerFactory.getLogger(BankController.class);
 
 	@PostMapping(path = "/login")
 	public ResponseEntity<String> login(@RequestBody Login user) {
